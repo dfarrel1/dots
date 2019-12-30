b92e8426816faebd6d9da4c185a6907d6e24755a
Dene Farrell <dene.farrell@latch.com>
2019-12-29 20:13:24 -0500

    find venv for freshen

--- profile/private.sh
+++ profile/private.sh
@@ -3,3 +3,7 @@
 
 repo_tools_dir="${GOPATH}/src/github.com/dfarrel1/repo-cross-talk"
 alias freshen="[ -d ${repo_tools_dir} ] && make -C ${repo_tools_dir} pull"
+
+# freshen needs to know where the pipenv virtual env is for repo-tools
+alias fresh_env='pew workon `pew ls | grep repo-cross-talk`'
+
