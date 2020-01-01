696d7498404408889226ef55ef089b256375279b
dene farrell <dene.farrell@latch.com>
2020-01-01 14:51:04 -0500

    fix freshen

--- profile/private.sh
+++ profile/private.sh
@@ -2,8 +2,13 @@
 # this will refer to non public tools
 
 repo_tools_dir="${GOPATH}/src/github.com/dfarrel1/repo-cross-talk"
-alias freshen="[ -d ${repo_tools_dir} ] && make -C ${repo_tools_dir} pull"
+
+
+freshen() {
+    repo_tools_env=`pew ls | tr ' ' '\n' | grep repo-cross-talk`
+    "[ -d ${repo_tools_dir} ] && pew in ${repo_tools_env} make -C ${repo_tools_dir} pull"
+}
 
 # freshen needs to know where the pipenv virtual env is for repo-tools
-alias fresh_env='pew workon `pew ls | grep repo-cross-talk`'
+alias fresh_env="pew workon `pew ls | tr ' ' '\n' | grep repo-cross-talk`"
 
