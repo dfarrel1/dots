50dbd7a4439952b21fb9362e523b9a66c61ebc54
dene farrell <dene.farrell@latch.com>
2020-02-23 14:11:20 -0500

    add full repo management

--- profile/private.sh
+++ profile/private.sh
@@ -4,9 +4,36 @@
 repo_tools_dir="${GOPATH}/src/github.com/dfarrel1/repo-cross-talk"
 
 # makes it easy to work on multiple machines on multiple repos
-freshen() {
+# uses pew: https://github.com/berdario/pew#usage
+# new virtual env:
+# (from $repo_tools_dir) 
+# pew new -r requirements.txt repo-cross-talk-env
+
+repo_prep() {
     repo_tools_env=`pew ls | tr ' ' '\n' | grep repo-cross-talk`
-    [ -d ${repo_tools_dir} ] && pew in ${repo_tools_env} make -C ${repo_tools_dir} pull
+    echo $repo_tools_env | [ ! $(wc -w) == 1 ] \
+        && echo "need one and only one env" \
+        && echo -e "envs: \n${repo_tools_env}" \
+        && return 1
+    echo "cross-talk venv looks good."
+    return 0
+}
+
+repo_act() {
+    repo_prep \
+    && [ -d ${repo_tools_dir} ] && pew in ${repo_tools_env} make -C ${repo_tools_dir} $1
+}
+
+pullall() {
+    repo_act "pull"
+}
+
+pushall() {
+    repo_act "push"
+}
+
+crossall() {
+    repo_act "cross"
 }
 
 help() {
