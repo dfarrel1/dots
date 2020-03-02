950ab4aac4fbfc74e781e11e27893c75303acc94
Dene Farrell <dene.farrell@latch.com>
2020-03-01 20:17:23 -0500

    avoid overshadowing JAVA_HOME

--- profile/java.sh
+++ profile/java.sh
@@ -3,9 +3,11 @@ alias sbr='mvn spring-boot:run'
 alias cr='cargorun'
 alias vcr='worldcom;jcr'
 
-export JAVA_HOME=$(/usr/libexec/java_home -v 12.0.2)
+export JAVA_HOME=$(/usr/libexec/java_home -v 11)
+# /Library/Java/JavaVirtualMachines/jdk-11.0.5.jdk/Contents/Home/
 
-eval "$(jenv init -)"
+# WARNING: loading jenv will clear JAVA_HOME
+# eval "$(jenv init -)"
 
 alias intellij='open -a "IntelliJ IDEA CE"'
 
