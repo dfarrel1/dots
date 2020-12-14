e9ce2a63f51a6371a40595810d525a47092b35dd
dfarrel1 <dene.farrell@gmail.com>
2020-12-14 11:16:29 -0500

    ssh

--- xtra/ssh/gitconfig.sh
+++ xtra/ssh/gitconfig.sh
@@ -3,5 +3,6 @@
 # repo config
 git config user.name dfarrel1
 git config user.email dene.farrell@gmail.com
-# git config user.name dlf-dds
-# git config user.email dene.farrell.ctr@friends.dds.mil
+
+# git config user.name <>
+# git config user.email <>
