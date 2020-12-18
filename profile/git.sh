7eb31cdca8a8c85150a43e2c371d1c73e9ec9e30
Dene Farrell <>
2020-12-18 17:38:58 -0500

    fix ssh aliasing breakage of browse()

--- profile/git.sh
+++ profile/git.sh
@@ -101,9 +101,13 @@ open_repo() {
   fi
 }
 
+alias2host() {
+  ssh -G $1 | awk '$1 == "hostname" { print $2 }'
+}
+
 browse() {
   repo_info -s || (echo "no repo found" && return 1)
-  current_path="$git_domain/$git_org/$git_repo"
+  current_path="$(alias2host $git_domain)/$git_org/$git_repo"
   [ ! -z $git_tree ] && [ $git_tree != "" ] && current_path="$current_path/tree/$git_branch/$git_tree" && [ ! -z $1 ] && current_path="$current_path/$1"
   open -a "Google Chrome" "http://$current_path"
 }
