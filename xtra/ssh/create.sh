9eedc356d7f0db2e1a643d2880b6a3d7b8230356
Dene Farrell <>
2020-12-18 16:49:15 -0500

    use a keyname arg

--- xtra/ssh/create.sh
+++ xtra/ssh/create.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
-KEY="id_rsa_<NAME_HERE>"
+usage()
+{
+    echo "usage: ./create.sh -o <output>"
+}
+
+if [ $# == 0 ]; then
+    usage
+    exit 1
+fi
+
+while getopts o: flag
+do
+    case "${flag}" in
+        o | --output ) KEY=${OPTARG};; 
+        * ) usage
+            exit 1       
+    esac
+done
+
+
+# KEY="id_rsa_<NAME_HERE>"
 ssh-keygen -f ~/.ssh/${KEY}
 chmod 600 ~/.ssh/${KEY}{,.pub}
 ssh-add ~/.ssh/${KEY}
