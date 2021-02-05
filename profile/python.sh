f7726e20a11eac15b0e642bfb80a3fbf74dd834f
dfarrel1 <dene.farrell@gmail.com>
2021-02-05 15:25:20 -0500

    manage pyenv

--- profile/python.sh
+++ profile/python.sh
@@ -6,6 +6,11 @@ alias python='/usr/local/bin/python3'
 alias pip='/usr/local/bin/pip3'
 # alias ansible='/Users/denefarrell/Library/Python/3.9/bin/ansible'
 
+eval "$(pyenv init -)"
+# For pyenv on mac OS 11
+export LDFLAGS="-L/usr/local/opt/zlib/lib -L/usr/local/opt/bzip2/lib"
+export CPPFLAGS="-I/usr/local/opt/zlib/include -I/usr/local/opt/bzip2/include"
+
 pipup() {
   curl https://bootstrap.pypa.io/get-pip.py | python
 }
