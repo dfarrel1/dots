4251475c6860841cb0e86a125fb169a56794ed35
dfarrel1 <dene.farrell@gmail.com>
2021-07-14 13:25:49 -0400

    create templated AWS Login items in 1pass and automatically create the aws profile entry in the ~/.aws/config file

--- profile/mac.sh
+++ profile/mac.sh
@@ -99,19 +99,36 @@ alias json='python -m json.tool'
 
 alias 1p="OP_CLOUD_ACCOUNT='dds' source ${HERE}/op_session.sh"
 
+get_list_of_aws_secrets() {
+    VAULT_NAME="dene"        
+    OP_CLOUD_ACCOUNT='dds'
+    SESSION_NAME="OP_SESSION_$OP_CLOUD_ACCOUNT"
+    eval "export ${SESSION_NAME}=$(1p session)"     
+    1p list items --vault=$VAULT_NAME --tags aws --categories login | jq -Mcr '.[].overview.title'
+}
+
 choose_aws_secret() {
-    local ARR=("exit")     
-    while read -r col1
-    do         
-        ARR+=("$col1")
-    done <"${HERE}/picklists/dds-aws"
+    local ARR=("exit")         
+    while read -r line; do                 
+        ARR+=( "$line" )        
+    done <<< $(get_list_of_aws_secrets)        
+    # to point to a file:
+    # done <"${HERE}/picklists/dds-aws"           
     [[ $# -lt 2 ]] && choice_set=`printf '%s\n' "${ARR[@]}"` && get_choice    
     local ON_FAILURE="echo \" \\\"${choice_set}\\\" not recognized. Exiting mfa.\";  return 1"
     { local SECRET_NAME=$choice_set; } || eval ${ON_FAILURE} 
     # choice globally stored as "$choice_set"
 }
 
-# this is redundant w/ aws-vault; will prob deprecate
+get_aws_acct_info() {
+    choose_aws_secret
+    VAULT_NAME="dene"        
+    OP_CLOUD_ACCOUNT='dds'
+    SESSION_NAME="OP_SESSION_$OP_CLOUD_ACCOUNT"
+    eval "export ${SESSION_NAME}=$(1p session)"     
+    export TMP_GLOBAL_ACCOUNT_INFO=`1p get item \"${choice_set}\" --vault=$VAULT_NAME | jq -Mcr '.details.sections[] | select(.title=="ACCOUNT_INFO").fields'`    
+}
+
 awskeys() {      
     choose_aws_secret
     VAULT_NAME="dene"        
@@ -145,7 +162,42 @@ mfa() {
     1p get totp "'"${SECRET_NAME}"'" | tr -d '\n' | pbcopy && pbpaste && echo '' 
 }
 
+newawslogin() {
+    OP_CLOUD_ACCOUNT='dds'
+    SESSION_NAME="OP_SESSION_$OP_CLOUD_ACCOUNT"
+    eval "export ${SESSION_NAME}=$(1p session)"    
+    1p create item login "$(op encode < ${HERE}/aws.template.json)" \
+        --vault dene \
+        --title "AWS-Template" \
+        --generate-password \
+        --tags "aws" \
+        --url https://signin.amazonaws-us-gov.com/console
+    
+}
 
+newawsprofile() {
+    get_aws_acct_info
+    ACCOUNT_ID=`echo $TMP_GLOBAL_ACCOUNT_INFO | jq -Mcr '.[] | select(.t=="ACCOUNT_ID") | .v'`
+    ACCOUNT_TYPE=`echo $TMP_GLOBAL_ACCOUNT_INFO | jq -Mcr '.[] | select(.t=="ACCOUNT_TYPE") | .v'`
+    AWS_PROFILE_NAME=`echo $TMP_GLOBAL_ACCOUNT_INFO | jq -Mcr '.[] | select(.t=="AWS_PROFILE_NAME") | .v'`
+    USER_NAME=`echo $TMP_GLOBAL_ACCOUNT_INFO | jq -Mcr '.[] | select(.t=="USER_NAME") | .v'`
+
+    SEARCH_STR="[profile ${AWS_PROFILE_NAME}]"
+    if grep -Fxq "${SEARCH_STR}" ~/.aws/config
+    then
+        echo "Profile already exists."
+        grep -Fn -A 4 "${SEARCH_STR}" ~/.aws/config
+    else
+        echo "Profile does not exist; creating now."
+        echo """
+        [profile ${AWS_PROFILE_NAME}]
+        region=us-gov-west-1
+        output=json
+        mfa_serial=arn:aws-us-gov:iam::${ACCOUNT_ID}:mfa/${USER_NAME}
+        """ | awk '{$1=$1};1' >> ~/.aws/config
+    fi
+
+}
 
 awslogin() {
     choose_aws_secret
@@ -164,7 +216,7 @@ awslogin() {
         *)
             export extra_chrome_opts=" ";;
     esac
-
+    # dummy arg tells it to use the choice_set already provided in env
     mfa dummy
     aws-vault --debug login ${AWS_VAULT_PROFILE_NAME} --stdout \
     | xargs -t /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome ${extra_chrome_opts} --new-window
