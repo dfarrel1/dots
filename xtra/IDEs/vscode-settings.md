2615fed2897f57b8a3dd3c7f4a2126d033025993
Dene Farrell <>
2021-08-31 13:26:40 -0400

    add newcomp.sh script to automatically configure computer, set up 1password, and download ssh keys and configure ssh

--- xtra/IDEs/vscode-settings.md
+++ xtra/IDEs/vscode-settings.md
@@ -11,6 +11,14 @@ gist id: a6bd450e6ce1355890c6c8bfafb737f3
 
 ---------------
 
+### turn on settings sync
+
+![vscode-settings-sync](vscode-settings-sync.png)
+
+... and follow the prompts
+
+---------------
+
 Shortcuts
 1. Upload Key : Shift + Alt + U
 2. Download Key : Shift + Alt + D
---
+++ xtra/newcomp.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# reminders:
+# set vscode settings: dots/IDES/vscode-settings.md
+# set cursor speed to max in terminal, keyboard settings
+
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+#brew stuff
+BREW_STATE_FILE=$SCRIPT_DIR/newcompstate_brewcomplete
+if [[ -f "$BREW_STATE_FILE" ]]; then
+    echo "$BREW_STATE_FILE exists."
+else
+    brew bundle install --file=${SCRIPT_DIR}/Brewfile
+    touch $BREW_STATE_FILE
+fi
+
+#OP stuff
+OP_STATE_FILE=$SCRIPT_DIR/newcompstate_opcomplete
+if [[ -f "$OP_STATE_FILE" ]]; then
+    echo "$OP_STATE_FILE exists."
+else
+    op_site="https://defense-digital-service.1password.com"
+    op_user="dene@dds.mil"
+    OP_CLOUD_ACCOUNT='dds'
+    op signin ${op_site} ${op_user} --shorthand ${OP_CLOUD_ACCOUNT}
+    touch $OP_STATE_FILE
+fi
+
+#ssh stuff
+SSH_STATE_FILE=$SCRIPT_DIR/newcompstate_sshcomplete
+if [[ -f "$SSH_STATE_FILE" ]]; then
+    echo "$SSH_STATE_FILE exists."
+else    
+    OP_CLOUD_ACCOUNT='dds'
+    SESSION_NAME="OP_SESSION_$OP_CLOUD_ACCOUNT"
+    eval "export ${SESSION_NAME}=$(op signin --account ${OP_CLOUD_ACCOUNT} --raw)"
+    VAULT_NAME="DDS-DENE-ssh"    
+    op_items=$(op list items --vault $VAULT_NAME | jq -Mcr '.[].overview.title' | sort)
+    for word in $op_items; do
+        echo $word              
+        op get document --vault $VAULT_NAME "$word" --output ~/.ssh/${word}
+    done
+
+    cat ~/.ssh/config
+    touch $SSH_STATE_FILE
+fi
